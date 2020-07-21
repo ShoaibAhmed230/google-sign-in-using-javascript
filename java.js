114b3f2fe8a287936233487481173a0cd1a15f59
Shoaib Ahmed <64016075+ShoaibAhmed230@users.noreply.github.com>
2020-07-21 18:05:30 +0500

    add functions

--- java.js
+++ java.js
@@ -1,28 +1,45 @@
 const email = "shebi345@gmail.com";
 function login(){
-    inputvalue = document.querySelector(".fxemail").value;
+    let  bordercolor = document.querySelector(".fxemail");
+    let errorspan = document.querySelector(".errspn");
+    let erroricon = document.querySelector(".fas");
+    let spanvalue = document.querySelector(".note");
+    let inputvalue = document.querySelector(".fxemail").value;
     if(inputvalue==email){
         window.location.href = "password.html";
-
-        alert("Congrates! your email is correct")
+        // alert("Congrates! your email is correct");
     }
-    else{
-        alert("your email isnot correct")
+ 
+    else{        
+       spanvalue.style.display="block";
+       erroricon.style.display="block";
+       errorspan.style.display="flex";
+       bordercolor.style.border="1px solid red";
     }
+    console.log(spanvalue);
     console.log(inputvalue);
 }
 const pass = "1234";
 
 function signin(){
+    let erricn = document.querySelector(".email i");
+    let  bordercolor = document.querySelector(".fxemail");
+    let errorspan = document.querySelector(".errspn");
+    let erroricon = document.querySelector(".fas");
+    let spanvalue = document.querySelector(".note");
     inputvalue = document.querySelector(".fxemail").value;
 
     if(inputvalue==pass){
         window.location.href = "welcome.html";
 
-        alert("your pass is correct")
+        // alert("your pass is correct");
 
     }
     else{
-        alert("your pass is incorrect");
+        erricn.style.display="block";
+        spanvalue.style.display="block";
+       erroricon.style.display="block";
+       errorspan.style.display="flex";
+       bordercolor.style.border="1px solid red";
     }
 }
